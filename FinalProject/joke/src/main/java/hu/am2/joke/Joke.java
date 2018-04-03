1b254d49daa7c87c57956bb01f61cb89587498f8
2ma <2ma.android@gmail.com>
2018-04-03 11:45:01 +0200

    added Joke library

---
+++ FinalProject/joke/src/main/java/hu/am2/joke/Joke.java
@@ -0,0 +1,10 @@
+package hu.am2.joke;
+
+public class Joke {
+
+    private static final String JOKE = "This is an awesome joke. Haha";
+
+    public String getJoke() {
+        return JOKE;
+    }
+}
